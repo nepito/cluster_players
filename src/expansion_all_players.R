5e7c820ff4165b5083813628fb3795ccc22bd79d
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-05-10 20:03:42 -0700

    ✂️ Divide in two

--- src/expansion_all_players.R
+++ src/expansion_all_players.R
@@ -40,31 +40,3 @@ cleaned_players %>%
   select("Player", "Position", grupos) %>%
   left_join(players_1, by = c("Player", "Position")) %>%
   write_csv("results/cleaned_LigaMX_1_with_grupo.csv")
-
-pca_liga_mx <- get_pca(cleaned_players)
-nombres <- paste0("PC", 1:7)
-names(pca_liga_mx$x[1:7]) <- nombres
-the_roles <- cleaned_players %>%
-  select(Player) %>%
-  cbind(as_tibble(pca_liga_mx$x[, 1:7]))
-
-the_roles_deciles <- the_roles %>%
-  mutate(PC1 = ntile(PC1, 10)) %>%
-  mutate(PC2 = ntile(PC2, 10)) %>%
-  mutate(PC3 = ntile(PC3, 10)) %>%
-  mutate(PC4 = ntile(PC4, 10)) %>%
-  mutate(PC5 = ntile(PC5, 10)) %>%
-  mutate(PC6 = ntile(PC6, 10)) %>%
-  mutate(PC7 = ntile(PC7, 10))
-
-deciles_to_plot <- the_roles_deciles %>%
-  pivot_longer(cols = 2:8, names_to = "PC", values_to = "deciles")
-
-larga <- the_roles_deciles %>%
-  pivot_longer(cols = 2:8, names_to = "PC", values_to = "deciles")
-aquino <- larga %>%
-  filter(Player == "J. Aquino")
-
-aquino %>% ggplot(aes(PC, deciles)) +
-  geom_col() +
-  coord_polar()
---
+++ src/pca_for_all_players.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(umap)
+# library(xgTools)
+set.seed(2)
+cleaned_players <- read_csv("results/cleaned_LigaMX_1_with_grupo.csv", show_col_types = FALSE)
+
+
+pca_liga_mx <- cleaned_players |>
+  get_pca()
+nombres <- paste0("PC", 1:7)
+names(pca_liga_mx$x[1:7]) <- nombres
+the_roles <- cleaned_players %>%
+  select(Player) %>%
+  cbind(as_tibble(pca_liga_mx$x[, 1:7]))
+
+the_roles_deciles <- the_roles %>%
+  mutate(PC1 = ntile(PC1, 10)) %>%
+  mutate(PC2 = ntile(PC2, 10)) %>%
+  mutate(PC3 = ntile(PC3, 10)) %>%
+  mutate(PC4 = ntile(PC4, 10)) %>%
+  mutate(PC5 = ntile(PC5, 10)) %>%
+  mutate(PC6 = ntile(PC6, 10)) %>%
+  mutate(PC7 = ntile(PC7, 10))
+
+deciles_to_plot <- the_roles_deciles %>%
+  pivot_longer(cols = 2:8, names_to = "PC", values_to = "deciles")
+
+larga <- the_roles_deciles %>%
+  pivot_longer(cols = 2:8, names_to = "PC", values_to = "deciles")
+aquino <- larga %>%
+  filter(Player == "J. Aquino")
+
+aquino %>% ggplot(aes(PC, deciles)) +
+  geom_col() +
+  coord_polar()
