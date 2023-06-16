a0b4646a0902f04d071eb27cad4bfc6541420f5c
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-15 20:51:42 -0700

    🧹 Clean code

--- src/expansion_all_players.R
+++ src/expansion_all_players.R
@@ -1,45 +1,37 @@
 library(tidyverse)
 library(umap)
+source("/workdir/R/select_variables.R")
 # library(xgTools)
 set.seed(2)
-players_1 <- read_csv("/workdir/tests/data/all_players.csv", show_col_types = FALSE)
+data_path <- "/workdir/tests/data/all_players.csv"
+players_1 <- read_csv(data_path, show_col_types = FALSE) |>
+  filter(`Minutes played` > 900)
 
-
-cleaned_players <- players_1 %>%
-  filter(`Minutes played` > 900 & Position != "GK")
-players_varaibles <- cleaned_players %>%
-  select(c(Player, "Minutes played", Position))
-metric <- "daves"
-variables <- cleaned_players %>%
+metric <- "soccerment"
+variables <- players_1 %>%
   select_variables[[metric]]()
 my_umap <- variables %>%
   scale() %>%
   umap()
 
-neighbors <- my_umap$knn$indexes[, 1:10]
-neighbors_distances <- my_umap$knn$distances[, 1:10]
-
-my_umap_10 <- umap.knn(indexes = neighbors, distances = neighbors_distances)
-
-
-my_umap <- cleaned_players %>%
-  select(-c(Player, "Minutes played", Position)) %>%
-  umap(n_neighbors = 10, knn = my_umap_10)
-
 datos <- tibble(x = my_umap$layout[, 1], y = my_umap$layout[, 2])
 n_grupos <- 5
 groups <- kmeans(datos, n_grupos)
 
+### Los jugadores a clasificar
+players_1$grupos <- factor(groups$cluster)
+
+predict.kmeans <- function(object, newdata){
+    centers <- object$centers
+    n_centers <- nrow(centers)
+    dist_mat <- as.matrix(dist(rbind(centers, newdata)))
+    dist_mat <- dist_mat[-seq(n_centers), seq(n_centers)]
+    max.col(-dist_mat)
+}
 datos$grupos <- factor(groups$cluster)
 ggplot(datos, aes(x = x, y = y, color = grupos)) +
   geom_point()
 ggsave(glue::glue("figurita_{metric}.png"))
 
-cleaned_players$grupos <- factor(groups$cluster)
-
-
-
-cleaned_players %>%
-  select("Player", "Position", grupos) %>%
-  left_join(players_1, by = c("Player", "Position")) %>%
-  write_csv(glue::glue("results/cleaned_LigaMX_1_with_grupo_{metric}.csv"))
+players_1 %>%
+  write_csv(glue::glue("results/clustered_macrogroup_with_{metric}.csv"))
