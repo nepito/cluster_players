320519f9f35b20103a6b2cba694e75ce403b5e87
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-15 20:52:37 -0700

    🧹 Clean code

--- src/second_cluster.R
+++ src/second_cluster.R
@@ -1,48 +1,39 @@
 library(tidyverse)
 library(umap)
-# library(xgTools)
+source("R/select_variables.R")
 set.seed(2)
-players_1 <- read_csv("/workdir/results/cleaned_LigaMX_1_with_grupo_daves.csv", show_col_types = FALSE) |>
-  distinct()
+data_path <- "results/clustered_macrogroup_with_soccerment.csv"
+players_1 <- read_csv(data_path, show_col_types = FALSE) |>
+  distinct() |>
+  filter(`Minutes played` > 900)
 
-macro_grupo <- 4
+macro_grupo <- 5
 
-cleaned_players <- players_1 %>%
-  filter(`Minutes played` > 900 & Position != "GK") |>
-  filter(grupos == macro_grupo)
-players_varaibles <- cleaned_players %>%
-  select(c(Player, "Minutes played", Position))
-metric <- "central_attackers"
-variables <- cleaned_players %>%
+metric <- "central_midfielder"
+variables <- players_1 %>%
   select_variables[[metric]]()
 my_umap <- variables %>%
   scale() %>%
   umap()
 
-neighbors <- my_umap$knn$indexes[, 1:10]
-neighbors_distances <- my_umap$knn$distances[, 1:10]
-
-my_umap_10 <- umap.knn(indexes = neighbors, distances = neighbors_distances)
-
-
-my_umap <- cleaned_players %>%
-  select(-c(Player, "Minutes played", Position)) %>%
-  umap(n_neighbors = 10, knn = my_umap_10)
-
 datos <- tibble(x = my_umap$layout[, 1], y = my_umap$layout[, 2])
 n_grupos <- 3
 groups <- kmeans(datos, n_grupos)
 
+
+predict.kmeans <- function(object, newdata){
+    centers <- object$centers
+    n_centers <- nrow(centers)
+    dist_mat <- as.matrix(dist(rbind(centers, newdata)))
+    dist_mat <- dist_mat[-seq(n_centers), seq(n_centers)]
+    max.col(-dist_mat)
+}
 datos$grupos <- factor(groups$cluster)
 ggplot(datos, aes(x = x, y = y, color = grupos)) +
   geom_point()
 ggsave(glue::glue("figurita_second_{metric}.png"))
 
-cleaned_players$s_grupos <- factor(groups$cluster)
-
-
+players_1$s_grupos <- factor(groups$cluster)
 
 cleaned_players %>%
-  select("Player", "Position", s_grupos) %>%
-  left_join(players_1, by = c("Player", "Position")) %>%
-  write_csv(glue::glue("results/cleaned_all_players_second_with_grupo_{metric}.csv"))
+  write_csv(glue::glue("results/second_clustered_with_{metric}.csv"))
