f41c99348aca671f4b8af71c19a0ca7acfe3d682
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-16 23:39:59 -0700

    🚧 Do not remove columns

--- src/concatenate_all_players.R
+++ src/concatenate_all_players.R
@@ -3,5 +3,4 @@ library("comprehenr")
 csv_files <- list.files("/workdir/data/", pattern = "csv$")
 all_players_paths <- to_vec(for (file in csv_files) glue::glue("/workdir/data/{file}"))
 all_players <- read_csv(all_players_paths, show_col_types = FALSE) |>
-  select(-c(2, 4:7)) |>
   write_csv("/workdir/tests/data/all_players.csv")
