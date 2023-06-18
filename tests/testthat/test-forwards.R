f4030cbba48f36eabe89b51eac19f04e4e1bd21a
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-18 15:34:51 -0700

    🛑🧪 Test `get_principal_variables()`: Add expectation

--- tests/testthat/test-forwards.R
+++ tests/testthat/test-forwards.R
@@ -13,7 +13,14 @@ describe("Get the principal variables", {
     subgroup_path <- "/workdir/tests/data/data_2_daves_3.csv"
     players <- readr::read_csv(subgroup_path, show_col_types = FALSE)
     obtained <- get_principal_variables(players)
-    expected <- c()
+    expected <- c(
+      "Passes to penalty area per 90", "Passes to final third per 90", "Progressive passes per 90",
+      "Through passes per 90", "Lateral passes per 90", "Long passes per 90",
+      "Progressive runs per 90", "Back passes per 90", "Dribbles per 90",
+      "Crosses to goalie box per 90", "Crosses per 90", "Shots blocked per 90",
+      "Interceptions per 90", "Touches in box per 90",
+      "Shots per 90"
+    )
     expect_equal(obtained, expected)
   })
 })
