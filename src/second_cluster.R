e4a0b17928d2b43c091f12f1e219ba0261e36b9f
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-18 22:46:50 -0700

    🚧 Add second type of cluster

--- src/second_cluster.R
+++ src/second_cluster.R
@@ -2,15 +2,15 @@ library(tidyverse)
 library(umap)
 source("R/select_variables.R")
 set.seed(2)
-macro_grupo <- 4
+macro_grupo <- 5
 data_path <- "results/clustered_macrogroup_with_soccerment.csv"
 players_1 <- read_csv(data_path, show_col_types = FALSE) |>
   distinct() |>
   filter(grupos == macro_grupo) |>
   filter(`Minutes played` > 900)
 
-
-metric <- "central_attackers"
+metrics <- c("", "daves", "", "central_attackers", "central_midfielder")
+metric <- metrics[macro_grupo]
 variables <- players_1 %>%
   select_variables[[metric]]()
 my_umap <- variables %>%
