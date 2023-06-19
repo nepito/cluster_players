7095b0576eb5cc72d7bd611b933c13ab38658f9d
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-18 23:12:55 -0700

    🛑🧪 Test `obtain_group_from_name()`

--- tests/testthat/test-find_a_player.R
+++ tests/testthat/test-find_a_player.R
@@ -1,11 +1,17 @@
 describe("Find macro group", {
   it("From 'results/clustered_macrogroup_with_soccerment.csv'", {
-data_path <- "/workdir/results/clustered_macrogroup_with_soccerment.csv"
-players_1 <- readr::read_csv(data_path, show_col_types = FALSE) |>
-  distinct()
+    data_path <- "/workdir/results/clustered_macrogroup_with_soccerment.csv"
+    players_1 <- readr::read_csv(data_path, show_col_types = FALSE) |>
+      distinct()
     path_of_expected <- "/workdir/tests/data/player_with_first_group.csv"
-    expected <- readr::read_csv(path_of_expected , show_col_types = FALSE)
-    obtained <- find_macro_group_for_all_players(data = "/workdir/results/clustered_macrogroup_with_soccerment.csv")
+    expected <- readr::read_csv(path_of_expected, show_col_types = FALSE)
+    obtained <- find_macro_group_for_all_players()
+    expect_equal(obtained, expected)
+  })
+  it("Return the name and the macrogroup from the name", {
+    player_name <- "Hee-Chan Hwang"
+    obtained <- obtain_group_from_name(player_name)
+    expected <- list("name" = "Hee-Chan Hwang", "macrogroup" = 4)
     expect_equal(obtained, expected)
   })
 })
