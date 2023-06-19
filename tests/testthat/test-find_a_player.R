c225e101b75c85314223953d11843c77deb0a746
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-18 22:49:35 -0700

    🪲✅ Fix bug in test

--- tests/testthat/test-find_a_player.R
+++ tests/testthat/test-find_a_player.R
@@ -1,10 +1,10 @@
 describe("Find macro group", {
   it("From 'results/clustered_macrogroup_with_soccerment.csv'", {
 data_path <- "/workdir/results/clustered_macrogroup_with_soccerment.csv"
-players_1 <- dplyr::read_csv(data_path, show_col_types = FALSE) |>
+players_1 <- readr::read_csv(data_path, show_col_types = FALSE) |>
   distinct()
-    path_of_expected <- "tests/data/player_with_first_group.csv"
-    expected <- dplyr::read_csv(path_of_expected , show_col_types = FALSE)
+    path_of_expected <- "/workdir/tests/data/player_with_first_group.csv"
+    expected <- readr::read_csv(path_of_expected , show_col_types = FALSE)
     obtained <- find_macro_group_for_all_players(data = "/workdir/results/clustered_macrogroup_with_soccerment.csv")
     expect_equal(obtained, expected)
   })
