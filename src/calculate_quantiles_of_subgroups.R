bb66477f0c3fbbdef149d5158b4f125bd253162b
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-19 19:14:17 -0700

    ♻️ Use `obtain_subgroup_from_name()`

--- src/calculate_quantiles_of_subgroups.R
+++ src/calculate_quantiles_of_subgroups.R
@@ -4,8 +4,14 @@ source("/workdir/R/expansion_all_players.R")
 source("/workdir/R/select_variables.R")
 source("/workdir/R/tyoe_of_variable.R")
 source("/workdir/R/forwards.R")
-sub_group <- 3
-players <- read_subgroup_of_players("results/second_clustered_macro_2_with_daves.csv", sub_group)
+source("/workdir/R/find_a_player.R")
+
+
+player_name <- "Hee-Chan Hwang"
+players_and_groups <- obtain_subgroup_from_name(player_name)
+sub_group <- players_and_groups[["subgroup"]]
+path_file <- players_and_groups[["path_file"]]
+players <- read_subgroup_of_players(path_file, sub_group)
 
 all_variables <- players |>
   get_principal_variables()
@@ -24,14 +30,3 @@ larga <- cuantiles_players[!duplicated(cuantiles_players$Player), ] |>
   pivot_longer(!Player, names_to = "variable", values_to = "deciles") |>
   left_join(tov) |>
   write_csv("results/larga_player.csv")
-
-n_var <- length(all_variables) + 1
-d_rice <- cuantiles_players[434, 2:n_var] |> as.numeric()
-dista <- c()
-for (i in 1:nrow(cuantiles_players)) {
-  player <- cuantiles_players[i, 2:n_var] |> as.numeric()
-  dista <- append(dista, sqrt(sum((player - d_rice)^2)))
-}
-cuantiles_players$dista <- dista
-cuantiles_players$index <- 1:nrow(cuantiles_players)
-cuantiles_players$year <- players$year
