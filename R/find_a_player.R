09ef69f02dc695930c49429c0b581351817fb837
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-19 18:10:09 -0700

    ♻️ Extract funcition `.filter_player_and_year()`

--- R/find_a_player.R
+++ R/find_a_player.R
@@ -12,9 +12,9 @@ obtain_group_from_name <- function(player_name) {
 
 obtain_subgroup_from_name <- function(player_name) {
   player_group <- obtain_group_from_name(player_name)
-  data_path = "/workdir/results/second_clustered_macro_4_with_central_attackers.csv"
+  data_path <- "/workdir/results/second_clustered_macro_4_with_central_attackers.csv"
   player_row <- find_macro_group_for_all_players(data_path, last_column = 116) |>
-    dplyr::filter(Player == player_name, year == 23)
+    .filter_player_and_year(player_name)
   return(
     list(
       "name" = player_row[["Player"]],
@@ -22,4 +22,9 @@ obtain_subgroup_from_name <- function(player_name) {
       "subgroup" = player_row[["s_grupos"]]
     )
   )
+}
+
+.filter_player_and_year <- function(player_row, player_name, season_year = 23) {
+  player_row |>
+    dplyr::filter(Player == player_name, year == season_year)
 }
