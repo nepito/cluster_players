7a809fbbd3f837d5e89763d6327b018965357fba
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-08-21 20:14:56 -0700

    ♻️🥈 Rename silver functions

--- R/find_league_name.R
+++ R/find_league_name.R
@@ -1,27 +1,27 @@
 find_league_name_from_list <- function(list_files) {
   list_files |>
-    extract_name_files() |>
-    extract_name_leagues()
+    .extract_name_files() |>
+    .extract_name_leagues()
 }
 
-extract_name_leagues <- function(name_files) {
+.extract_name_leagues <- function(name_files) {
   comprehenr::to_vec(for (name in name_files) stringr::str_split(name, "_[:digit:]{2}")[[1]][1])
 }
 
-extract_name_files <- function(list_files) {
+.extract_name_files <- function(list_files) {
   comprehenr::to_vec(for (file in list_files) stringr::str_split(file, "/")[[1]][4])
 }
 
 find_year_from_list <- function(list_files) {
   list_files |>
-    extract_last_year_from_list() |>
-    remove_csv_extension()
+    .extract_last_year_from_list() |>
+    .remove_csv_extension()
 }
 
-extract_last_year_from_list <- function(list_files) {
+.extract_last_year_from_list <- function(list_files) {
   comprehenr::to_vec(for (file in list_files) stringr::str_split(file, "-")[[1]][2])
 }
 
-remove_csv_extension <- function(list_files) {
+.remove_csv_extension <- function(list_files) {
   comprehenr::to_vec(for (file in list_files) stringr::str_split(file, "\\.")[[1]][1])
 }
