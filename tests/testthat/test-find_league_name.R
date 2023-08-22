34ada57f00d21c2b40744c504116ff704faa52b0
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-08-21 19:58:50 -0700

    🛑🧪 Test `find_year_from_list()`

--- tests/testthat/test-find_league_name.R
+++ tests/testthat/test-find_league_name.R
@@ -18,3 +18,12 @@ describe("find_league_name_from_list()", {
     expect_equal(obtained, expected)
   })
 })
+
+describe("find_year_from_list()", {
+  it("Gold 🪙", {
+    list_files <- c("/workdir/data/femenil_mx_20-21.csv", "/workdir/data/LigaMX_21-22.csv", "/workdir/data/ligue_1_22-23.csv")
+    obtained <- find_year_from_list(list_files)
+    expected <- c("21", "22", "23")
+    expect_equal(obtained, expected)
+  })
+})
