95089690214fe4cdf7f80740c74b3f59f566bfed
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-08-21 21:05:35 -0700

    🚧 Add league ids

--- src/concatenate_all_players.R
+++ src/concatenate_all_players.R
@@ -4,10 +4,12 @@ source("/workdir/R/find_league_name.R")
 csv_files <- list.files("/workdir/data/", pattern = "csv$")
 all_players_paths <- to_vec(for (file in csv_files) glue::glue("/workdir/data/{file}"))
 years <- find_year_from_list(all_players_paths)
+league_ids <- find_league_id_from_list(all_players_paths)
 all_players <- tibble::tibble()
 for (i in 1:length(all_players_paths)) {
   new_league <- read_csv(all_players_paths[i], show_col_types = FALSE) |>
-    add_column(year = years[i])
+    add_column(year = years[i]) |>
+    add_column(league_id = league_ids[i])
   all_players <- rbind(all_players, new_league)
   print(all_players_paths[i])
 }
