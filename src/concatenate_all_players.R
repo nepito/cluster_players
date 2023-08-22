17710a5d45f418b1c82e0276063139b99bacb3e2
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-08-21 20:32:08 -0700

    ♻️ Use `find_league_name`

--- src/concatenate_all_players.R
+++ src/concatenate_all_players.R
@@ -1,14 +1,15 @@
 library("tidyverse")
 library("comprehenr")
+source("/workdir/R/find_league_name.R")
 csv_files <- list.files("/workdir/data/", pattern = "csv$")
 all_players_paths <- to_vec(for (file in csv_files) glue::glue("/workdir/data/{file}"))
-years_and_csv <- to_vec(for (year in all_players_paths) str_split(year, "-")[[1]][2])
-years <- to_vec(for (year in years_and_csv) str_split(year, "\\.")[[1]][1])
+years <- find_year_from_list(all_players_paths)
 all_players <- tibble::tibble()
 for (i in 1:length(all_players_paths)) {
   new_league <- read_csv(all_players_paths[i], show_col_types = FALSE) |>
     add_column(year = years[i])
   all_players <- rbind(all_players, new_league)
+  print(all_players_paths[i])
 }
 
 all_players |> write_csv("/workdir/tests/data/all_players.csv")
