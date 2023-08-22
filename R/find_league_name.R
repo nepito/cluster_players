1389f767caebfe24da636168188249330482c5cb
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-08-21 20:03:57 -0700

    ✅🥈 Pass `extract_last_year_from_list()`

--- R/find_league_name.R
+++ R/find_league_name.R
@@ -11,3 +11,7 @@ extract_name_leagues <- function(name_files) {
 extract_name_files <- function(list_files) {
   comprehenr::to_vec(for (file in list_files) stringr::str_split(file, "/")[[1]][4])
 }
+
+extract_last_year_from_list <- function(list_files) {
+  comprehenr::to_vec(for (file in list_files) stringr::str_split(file, "-")[[1]][2])
+}
