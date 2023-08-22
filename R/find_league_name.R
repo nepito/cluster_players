830698e021eb2e94b4a579d31a90f5a6d7dfff2b
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-08-21 19:52:12 -0700

    ✅ Pass test

---
+++ R/find_league_name.R
@@ -0,0 +1,13 @@
+find_league_name_from_list <- function(list_files) {
+  list_files |>
+    extract_name_files() |>
+    extract_name_leagues()
+}
+
+extract_name_leagues <- function(name_files) {
+  comprehenr::to_vec(for (name in name_files) stringr::str_split(name, "_[:digit:]{2}")[[1]][1])
+}
+
+extract_name_files <- function(list_files) {
+  comprehenr::to_vec(for (file in list_files) stringr::str_split(file, "/")[[1]][4])
+}
